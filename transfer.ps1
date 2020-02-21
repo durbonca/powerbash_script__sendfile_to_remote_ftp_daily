b019503d6c4d9e3dcdc1824c81b4b6541f06a7b0
durbonca <durbonca@gmail.com>
2020-02-21 14:54:50 -0300

    take files from date

--- transfer.ps1
+++ transfer.ps1
@@ -1,9 +1,10 @@
+$date = (Get-Date).adddays(-1)
 $source = "C:\Users\Manuel Duran\Documents\bash-test\test"
 $destination = "ftp://durbonca:261187@192.168.50.115/test"
 
 $webclient = New-Object -TypeName System.Net.WebClient
 
-$files = Get-ChildItem $source
+$files = Get-ChildItem $source | Where-Object {($_.LastWriteTime -ge $date)}
 
 foreach ($file in $files)
 {
@@ -12,5 +13,7 @@ foreach ($file in $files)
 } 
 
 $webclient.Dispose()
+
+
 
 pause
